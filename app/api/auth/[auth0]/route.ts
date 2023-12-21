d77845dba41f5697eafa75a0ddbc0cd9a62f3ad9
Naoise Golden Santos <naoise.golden@gmail.com>
2023-12-21 10:35:00 +0100

    Make it work

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -3,7 +3,7 @@ import {handleAuth, handleLogin} from '@auth0/nextjs-auth0';
 export const GET = handleAuth({
     login: handleLogin({
         authorizationParams: {
-            audience: 'http://localhost:8081', // or AUTH0_AUDIENCE
+            // audience: 'http://localhost:8081', // or AUTH0_AUDIENCE
         },
         returnTo: "/profile"
     })
